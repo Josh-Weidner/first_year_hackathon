fdc3baf4317c85c829af122e96f5a468f952befc
Josh-Weidner <josh.weidner2@gmail.com>
2025-10-18 01:30:06 -0600

    Create questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Features from "./pages/Features.tsx";
+import Questions from "./pages/Questions.tsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Features" element={<Features />} />
-      <Route path="/Features" element={<Features />} />
+      <Route path="/Questions" element={<Questions />} />
     </Routes>
   );
 }
